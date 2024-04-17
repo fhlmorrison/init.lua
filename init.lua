f403aa3ba8e9cbd94ca8be0642976908bd4a3be3
Fraser Morrison <frazemor@hotmail.com>
2024-04-17 17:08:50 -0400

    Added keybinds

--- init.lua
+++ init.lua
@@ -113,7 +113,7 @@ vim.opt.showmode = false
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
+-- vim.opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -903,6 +903,47 @@ require('lazy').setup({
     },
   },
 })
+
+
+-- My keybinds
+
+-- Project View - Explorer
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Move highlighted lines up and down (alt+up/down in VSCode)
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in position when appending next line (J)
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- Do not copy on replace (keeps pasted in copy buffer)
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- Yank into system clipboard
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+
+-- Delete without copying
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+
+-- Format document with lsp
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+
+-- Quick fix navigation
+vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+
+-- Buffer navigation
+vim.keymap.set('n', '<leader>bn', '<cmd>bn<CR>')
+vim.keymap.set('n', '<leader>bp', '<cmd>bp<CR>')
+
+-- Replace current word
+vim.keymap.set('n', '<C-h>', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set('v', '<C-h>', [[y:%s/\<<C-r>0\>/<C-r>0/gI<Left><Left><Left>]])
+
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
